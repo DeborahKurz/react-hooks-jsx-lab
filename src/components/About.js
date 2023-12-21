82e33fa6e864b5443d0c300b8a770388ae5e6ecb
Deborah Kurz <DeborahKurz6@gmail.com>
2023-12-21 10:30:07 -0700

    Complete Lab

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ import { image } from "../data/data";
 function About() {
   return (
     <div id="about">
-
+      <h2>About Me</h2>
+      <p>Hello world!</p>
+      <img src={image} alt="I made this"></img>
     </div>
   )
 }
